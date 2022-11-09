cbf055be06cc28ba72f89e41bc579bbef39c0f16
mussasso <darouich.mm@hotmail.com>
2022-11-09 12:18:14 +0100

    auth sans avatar

--- app/Models/User.php
+++ app/Models/User.php
@@ -19,6 +19,9 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'name',
+        'lastname',
+        'age',
+        'avatar',
         'email',
         'password',
     ];
